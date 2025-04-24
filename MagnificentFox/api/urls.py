095dfcb98527964e706d2bd95de715420bd44986
Ankit <ankit.ap13@gmail.com>
2025-04-25 00:39:50 +0530

    added more apis

--- MagnificentFox/api/urls.py
+++ MagnificentFox/api/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
     path('users/', views.UserListCreate.as_view(), name='user-list-create'),
     path('products/', views.ProductListCreate.as_view(), name='product-list-create'),
     path('orders/', views.OrderListCreate.as_view(), name='order-list-create'),
-    path('columnStructureData/', views.column_structure_data, name='column-structure-data')
+    path('columnStructureData/', views.column_structure_data, name='column-structure-data'),
+    path('list-products/', views.list_products, name='list-products'),
+    path('card-list-data/', views.card_list_data, name='card-list-data'),
+    path('favourites-data/', views.favourites_data, name='favourites-data'),
+    path('reviews-data/', views.reviews_data, name='reviews-data'),
+    path('video-cards-data/', views.video_cards_data, name='video-cards-data')
 ]
